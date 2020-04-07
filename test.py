464462304ec3e7cdf64e89817da32104406950ec
Colin McGovern <colin.mcgovern@gmail.com>
2020-04-07 10:04:21 +0100

    Added feedback from ideas

--- test.py
+++ test.py
@@ -6,6 +6,15 @@ key="""b4700256516b6e3ea2ed3aadd995e32b7fa156a73f7326aa0fe93cdee9c3e678"""
 
 test = Prodpad(key)
 
-idea = test.idea('729942')
-
+print("Getting idea")
+print("############")
+idea = test.idea('3504')
 print(idea)
+print("############\n\n")
+print("Getting feedback")
+print("############")
+feedback_items = idea.getFeedback()
+
+for feedback in feedback_items:
+	print(feedback)
+print("############\n\n")
