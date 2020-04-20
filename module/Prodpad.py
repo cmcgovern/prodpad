0093703855b060a51a96473f81450e40524b1bb2
Colin McGovern <colin.mcgovern@gmail.com>
2020-04-20 17:07:01 +0100

    Clean up

--- module/Prodpad.py
+++ module/Prodpad.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from module.Resources import Idea
+from module.Resources import Roadmaps
+from module.Resources import ProductLines
 
 class Prodpad(object):
 	"""
@@ -13,13 +15,49 @@ class Prodpad(object):
 		super(Prodpad, self).__init__()
 		self.key = key
 		self.headers = {"""Authorization""": """Bearer """ + key}
+
 	
-	def idea(self, id):
+	def idea(self, **kwargs):
 		"""docstring for idea"""
-		idea = Idea(id, self)
-		
+		for key, value in kwargs.items():
+			if key == 'short_id':
+				idea = Idea(self, short_id=value)
+			if key == 'id':
+				idea= Idea(self, id=value)
 		return idea
 
+
+	def roadmaps(self, **kwargs):
+		"""docstring for allroadmaps"""
+		roadmaps = None
+		my_id = None
+		for key, value in kwargs.items():
+			if key == 'id':
+				my_id = value
+				roadmaps = Roadmaps(self, id=my_id)
+		
+		if my_id is None:
+			roadmaps = Roadmaps(self)
+		return roadmaps
+
+	
+	def product_lines(self, **kwargs):
+		"""docstring for product_lines"""
+		# Whatever happens, we return a list
+		product_lines = []
+		
+		my_id = None
+		for key, value in kwargs.items():
+			if key == 'id':
+				my_id = value
+				product_lines.append(ProductLines(self, id=my_id))
+			if key == 'name':
+				my_name = value
+				product_lines.append(ProductLines(self, name=my_name))
+		
+		if my_id is None and my_name is None:
+			product_lines = ProductLines(self)
+		return product_lines
 
 
 
--- module/Resources.py
+++ module/Resources.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import requests
 import json
+		
 
 class Resource(object):
 	"""Generic Resource Class"""
-	
 	def __init__(self, session, endpoint):
 		super(Resource, self).__init__()
 		self.session = session
@@ -21,29 +21,102 @@ class Resource(object):
 				url += '/' + str(value)
 			if key == 'params':
 				params = value
-			if key == 'add_to_url':
+			if key == 'add_path_to_url':
 				url += '/' + value
 
 		response = requests.get(url, headers=self.session.headers, params=params)
+		print(response.text)
 		if (response.status_code == requests.codes.ok):
-			response_dict = json.loads(response.text)
+			response_dict = json.loads(response.text) 
 		
 		return response_dict
 
 
-class Idea(Resource):
-	"""A Prodpad Idea"""
+
+class Company(Resource):
+	"""docstring for Company"""
+	def __init__(self, arg):
+		super(Company, self).__init__(session, """roadmaps""")
+		self.product_lines = product_lines
+		
+
+
+class ProductLine(Resource):
+	"""docstring for ProductLine"""
 	
+	def getProductLineById(self, id, product_lines):
+		"""docstring for getProductLineById"""
+		for product_line in product_lines:
+			if product_line['id'] == id:
+				return product_line
+		return None
+	
+	
+	def getProductLineByName(self, name, product_lines):
+		"""docstring for getProductLineByName"""
+		for product_line in product_lines:
+			if product_line['name'] == name:
+				return product_line
+		return None
+	
+	
+	def __init__(self, session, **kwargs):
+		super(ProductLine, self).__init__(session, """roadmaps""")
+		self.id = None
+		self.name = None
+		
+		for key, value in kwargs.items():
+			if key == 'id':
+				self.id = value
+				product_line_dict = getProductLineByName(self.id, roadmap_dict['product_lines'])
+			if key == 'name':
+				self.name = value
+				product_line_dict = getProductLineByName(self.name, roadmap_dict['product_lines'])
+		
+		roadmap_dict = self.loadFromAPI()
+		
+
+
+class Roadmap(Resource):
+	"""docstring for Roadmap"""
+	
+	def __init__(self, session, id):
+		super(Roadmaps, self).__init__(session, """roadmaps""")
+		self.swimlanes = []
+		self.id = id
+		roadmap_dict = self.loadFromAPI(id=self.id)
+	
+	def __str__(self):
+		roadmap_string = """Roadmap ID: """ + str(self.id) + """, swimlanes: """ + str([str(swimlane) for swimlane in self.swimlanes])
+		return roadmap_string
+		
+
+
+class RoadmapCard(Resource):
+	"""A roadmap card cannot be queried directly from the API"""
+	def __init__(self, card_dict):
+		super(RoadmapCard, self).__init__(None, None)
+		self.id = card_dict['id']
+		self.title = card_dict['title']
+		self.ideas = []
+	
+	def __str__(self):
+		card_string = str(self.title) + """ #Ideas: """ + str(len(self.ideas))
+		return card_string
+
+
+
+class Idea(Resource):	
 	"""If launched with no ID, then don't query the API"""
 	def __init__(self):
-		super(Idea, self).__init__(session,"""ideas""")
+		super(Idea, self).__init__(None, None)
 	
 	
 	def getFeedback(self):
 		"""docstring for getAllFeedback"""
 		linked_feedback = []
 		
-		feedback_items_dict = self.loadFromAPI(id=self.id, add_to_url="""feedback""")
+		feedback_items_dict = self.loadFromAPI(id=self.id, add_path_to_url="""feedback""")
 		for feedback_item in feedback_items_dict:
 			linked_feedback.append(Feedback(feedback_item, self, self.session))
 		
@@ -51,15 +124,26 @@ class Idea(Resource):
 	
 	
 	"""If launched with an ID, then query the API for this idea"""
-	def __init__(self, id, session):
+	def __init__(self, session, **kwargs):
 		super(Idea, self).__init__(session,"""ideas""")
-		self.short_id = id
-		self.options = {'by_project_id': 'true', 'expand': 'true'} # This allows us to use the visible ID to retrieve ideas
-		idea_dict = self.loadFromAPI(id=self.short_id, params=self.options)
+		for key, value in kwargs.items():
+			if key == 'short_id':
+				self.short_id = value
+				self.options = {'by_project_id': 'true', 'expand': 'true'} # This allows us to use the visible ID to retrieve ideas
+			if key == 'id':
+				self.id = value
+				self.options = {'expand': 'true'}
+		
+		self.roadmap_cards = []
+		
+		# Call API and fill in details
+		idea_dict = self.loadFromAPI(id=(self.short_id if self.short_id is not None else self.id), params=self.options)
 		
 		if idea_dict is not None:
 			self.title = idea_dict['title']
 			self.id = idea_dict['id']
+			for roadmap_card in idea_dict['roadmap_cards']:
+				self.roadmap_cards.append(RoadmapCard(roadmap_card))
 	
 	
 	"""Human-readable string"""
@@ -78,16 +162,11 @@ class Feedback(Resource):
 	linked_ideas = []
 	
 	"""If launched with no ID, then don't query the API"""
-	def __init__(self):
-		super(Idea, self).__init__(session,"""feedbacks""")
-	
-	
-	"""Get all feedback for an idea"""
 	def __init__(self, feedback_dict, idea, session):
 		super(Feedback, self).__init__(session,"""feedbacks""")
 		self.linked_ideas.append(idea)
 		self.id = feedback_dict['id']
-		self.dict = feedback.dict
+		self.dict = feedback_dict
 	
 	
 	"""Human-readable string"""
